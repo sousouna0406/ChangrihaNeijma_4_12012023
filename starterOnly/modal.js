690fc0a196e75510a892dcb312875f70f03e4857
changriha neijma <changriha.neijma@orange.fr>
2023-01-23 00:10:33 +0100

    fermeture form

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,3 +1,5 @@
+// Fonction pour menu responsive
+
 function editNav() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
@@ -6,18 +8,36 @@ function editNav() {
     x.className = "topnav";
   }
 }
+// Élements du DOM
 
-// DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const closeForm = document.querySelector(".close")
 
-// launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+const form = document.getElementById("form")
+const submit = document.querySelector(".btn-submit")
+const dataForm = document.querySelectorAll(".formData")
+const firstName = document.getElementById("first")
+const lastName = document.getElementById("last")
+const email = document.getElementById("email")
+const date = document.getElementById("date")
 
-// launch modal form
+/* Évenements formulaire ouverture et fermeture du formulaire */
+
+// Fonction d'ouverture du formulaire
 function launchModal() {
   modalbg.style.display = "block";
 }
+// Fonction de fermeture du formulaire
+ function closeMyForm() {
+  modalbg.style.display="none";
+}
+// methode qui appel les fonctions d'ouverture et de fermeture du formulaire
+modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+closeForm.addEventListener("click", closeMyForm)
+
+/* Recuperation des données du formulaire et regex */
+
 
 
