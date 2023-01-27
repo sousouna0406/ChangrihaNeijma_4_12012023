19471d5d753156b5954a0fd882d01217edc7a4b4
changriha neijma <changriha.neijma@orange.fr>
2023-01-27 01:03:03 +0100

    msg error

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -25,7 +25,10 @@ const date = document.getElementById("date")
 const nameError = document.getElementById("NameErrorMsg")
 const nameErrorlast = document.getElementById("NameErrorMsglast")
 const msgErrorEmail = document.getElementById("emailErrorMsg")
-
+const birthday = document.getElementById("birthdate")
+const msgErrorBirthday = document.getElementById("birthdayErrorMsg")
+const quantity = document.getElementById("quantity")
+const msgErrorQuantity =document.getElementById("quantityErrorMsg")
 const btnValid = document.querySelector(".btn-submit")
 /* Évenements formulaire ouverture et fermeture */
 
@@ -41,6 +44,7 @@ function launchModal() {
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 closeForm.addEventListener("click", closeMyForm)
 
+/* Regex Email/Prenom/nom*/
 
 const regexName =(value)=>{
   return /^(?=.{2,})([a-zA-Z\s-])+$/.test(value);
@@ -50,64 +54,106 @@ const regeXemail = (value) => {
     value
   );
 };
-
+// Paragraphe d'erreur masqué
 nameError.style.display="none"
 nameErrorlast.style.display="none"
 msgErrorEmail.style.display="none"
+msgErrorQuantity.style.display="none"
 
-firstName.addEventListener("change", function(label){
-  const lePrenom = firstName.value;
-   if (regexName(lePrenom)) {
-    console.log("ok")
-    nameError.style.display="none"  
-    firstName.style.color="green"  
+// FONCTION POUR TEST DU PREMON
+
+firstName.addEventListener("change", function() {
+  const theFirstName = firstName.value;
+   if (regexName(theFirstName)) {
+    nameError.style.display="none"
+    firstName.style.color="green"
+    firstName.style.border= "green solid 3px" 
    } else {
-    console.log("ko")
     nameError.style.display="block"
     nameError.innerHTML = "Veuillez entrer deux caractères minimum"
-    nameError.style.fontSize ="15px"
+    nameError.style.fontSize ="13px"
     nameError.style.color ="red"
     firstName.style.color="red"
+    firstName.style.border= "red solid 2px"
    }
 })
-lastName.addEventListener("change", function(input,label){
-  const leNom = lastName.value;
-  if (regexName(leNom)) {
+
+// FONCTION POUR TEST DU NOM
+
+lastName.addEventListener("change", function() {
+  const theLastname = lastName.value;
+  if (regexName(theLastname)) {
    console.log("ok")
    lastName.style.color="green"
-   nameErrorlast.style.display="none"    
+   nameErrorlast.style.display="none"   
+   lastName.style.border= "green solid 3px" 
   } else {
    console.log("ko")
     nameErrorlast.style.display="block"
     nameErrorlast.innerHTML = "Veuillez entrer deux caractères minimum"
-    nameErrorlast.style.fontSize ="15px"
+    nameErrorlast.style.fontSize =" 13px"
     nameErrorlast.style.color ="red"
+    lastName.style.border= "red solid 2px"
    lastName.style.color="red"
-   //errorFirstName.innerHTML = "Veuillez entrer deux caractères minimum"
   }
 })
 
-email.addEventListener("change", function(input,label){
-  const lemail = email.value;
-  if (regeXemail(lemail)) {
+// FONCTION POUR TEST DE L EMAIL
+
+email.addEventListener("change", function() {
+  const theEmail = email.value;
+  if (regeXemail(theEmail)) {
    console.log("ok")
    email.style.color="green"
    msgErrorEmail.style.display="none" 
-   
+   email.style.border= "green solid 3px"  
   } else {
    console.log("ko")
    msgErrorEmail.style.display="block"
    msgErrorEmail.innerHTML = "Veuillez entrer un email valide"
-   msgErrorEmail.style.fontSize ="15px"
+   msgErrorEmail.style.fontSize ="13px"
    msgErrorEmail.style.color ="red"
    email.style.color="red"
+   email.style.border= "red solid 2px"
   }
 })
 
+// FONCTION POUR ANNIVERSAIRE
 
+birthday.addEventListener("change", function () {
+  
+if (!birthday ) {
+  msgErrorBirthday.style.display="block"
+  msgErrorBirthday.innerHTML = "Veuillez saisir une date"
+  msgErrorBirthday.style.fontSize ="13px"
+  msgErrorBirthday.style.color ="red"
+  birthday.style.color="red"
+  birthday.style.border= "red solid 2px"
+}else{
+  console.log("KO")
+  birthday.style.color="green"
+   msgErrorBirthday.style.display="none" 
+   birthday.style.border= "green solid 3px"  
+}
+})
 
+// FONCTION POUR NOMBRE DE TOURNOIS
 
-
+ quantity.addEventListener("change", function () {
+  const quantityValue = quantity.value
+  if (quantityValue < 0|| quantityValue >99) {
+    msgErrorQuantity.style.display="block"
+    msgErrorQuantity.innerHTML = "Veuillez entrer un nombre entre 0 et 99"
+    msgErrorQuantity.style.fontSize ="13px"
+    msgErrorQuantity.style.color ="red"
+    quantity.style.color="red"
+    quantity.style.border= "red solid 2px"
+  } else {
+    quantity.style.color="green"
+    msgErrorQuantity.style.display="none" 
+    quantity.style.border= "green solid 3px"  
+  }
+ })
 
 
 
